cfc4f966c330049ac41facde81d0004be47748d8
Badal Prasad Singh <badalstar1806@gmail.com>
2024-06-22 00:20:29 +0530

    added default port 8000 to AI-Agents

--- AI-Agents/app.py
+++ AI-Agents/app.py
@@ -71,4 +71,6 @@ async def agents(query_request: QueryRequest):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="localhost", port=8000)
+
+    port = int(os.environ.get('PORT', 8000))
+    uvicorn.run(app, host='0.0.0.0', port=port)
